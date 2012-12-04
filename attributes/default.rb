96e1ec543bb11e07e3a588e22d7f5077fb23da28
Arthur Tumanyan <arthurtumanyan@yahoo.com>
2012-12-04 18:04:29 +0400

    Update attributes/default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -14,8 +14,8 @@ default['MONITIS']['AGENTNAME'] = node[:hostname]
 
 default['MONITIS']['TARBALL_32'] = "MonitisLinuxAgent-32bit.tar.gz"
 default['MONITIS']['TARBALL_64'] = "MonitisLinuxAgent-64bit.tar.gz"
-default['MONITIS']['TARBALL_URL_32'] = "http://www.monitissupport.com/agents/MonitisLinuxAgent-32bit.tar.gz"
-default['MONITIS']['TARBALL_URL_64'] = "http://www.monitissupport.com/agents/MonitisLinuxAgent-64bit.tar.gz"
+default['MONITIS']['TARBALL_URL_32'] = "http://dashboard.monitis.com/downloader?type=internalAgent&platform=linux32"
+default['MONITIS']['TARBALL_URL_64'] = "http://dashboard.monitis.com/downloader?type=internalAgent&platform=linux64"
 
 # overrides on a platform-by-platform basis
 case platform
@@ -33,6 +33,6 @@ default['MONITIS']['LIBSSL'] = "libopenssl1_0_0"
 when "windows"
 
 default['MONITIS']['TARBALL'] = "MonitisWindowsAgent-32-64bit.zip"
-default['MONITIS']['TARBALL_URL'] = "http://www.monitissupport.com/agents/MonitisWindowsAgent-32-64bit.zip"
+default['MONITIS']['TARBALL_URL'] = "http://dashboard.monitis.com/downloader?type=internalAgent&platform=win64"
 
 end
